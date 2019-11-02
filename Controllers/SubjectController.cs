4c54732d761af9fade908612b0b7837934351082
Pedro Capriles <caprilespe@outlook.com>
2019-11-02 18:50:34 -0300

    Partial views

--- Controllers/SubjectController.cs
+++ Controllers/SubjectController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using aspnetcore.coreescuela.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,14 +7,37 @@ namespace aspnetcore.coreescuela.Controllers
 {
     public class SubjectController : Controller
     {
-         public IActionResult Index()
+        public IActionResult MultiSubject()
+        {
+            var subjects = new List<Subject>(){
+                new Subject{
+                    Name = "Programación"
+                },
+                new Subject{
+                    Name = "Matemática"
+                },
+                new Subject{
+                    Name = "Educación Física"
+                },
+                 new Subject{
+                    Name = "Castellano"
+                },
+                new Subject{
+                    Name = "Ciencias Naturales"
+                }
+            };
+
+            ViewBag.CosaDinamica = "La Monja";
+            ViewBag.Date = DateTime.Now;
+            return View("MultiSubject", subjects);
+        }
+
+        public IActionResult Index()
         {
             var subject = new Subject
             {
                 Name = "Programación"
             };
-            ViewBag.CosaDinamica = "La Monja";
-            ViewBag.Date = DateTime.Now;
             return View(subject);
         }
     }
